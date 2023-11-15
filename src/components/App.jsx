8055a8c0bb909fe6d83b3ba32548b860aa5b5838
Leglocker01 <beristaindelamora@gmail.com>
2023-11-14 18:13:43 -0600

    adding the image uploader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { Score } from "../components/Score";
 import { Timer } from "./Timer";
+import { Image } from "./Image";
 
 export function App() {
   return (
     <>
       <Score />
       <Timer />
+      <Image />
     </>
   );
 }
---
+++ src/components/Image.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const Image = () => {
+  const [image, setImage] = useState(null);
+
+  // handle image uploads
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      setImage(e.target.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleImageUpload} />
+      {image && <img src={image} alt="Uploaded" style={{ maxWidth: "10%" }} />}
+    </div>
+  );
+};
