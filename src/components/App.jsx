2fd4abdcb90095b4c76841d8f0b5fbced39cde47
Leglocker01 <beristaindelamora@gmail.com>
2024-01-06 23:29:22 -0600

    deleting the basename

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import "../assets/App/App.css";
 export function App() {
   return (
     <>
-      <BrowserRouter basename="">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<AuthLayout />}>
             <Route index element={<Judge />} />
